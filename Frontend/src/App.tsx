1835eec7afd6259e8f79cb57f3b7968fa021309f
Başar Arda Baykal <baykalbasararda@gmail.com>
2025-11-29 15:35:35 +0300

    react router dom installed

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
 import { Giveaway } from './pages/Giveaway';
+import { Participate } from './pages/Participate';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <Giveaway />;
+  return (
+    <Routes>
+      <Route path="/" element={<Giveaway />} />
+      <Route path="/participate" element={<Participate />} />
+    </Routes>
+  );
 }
 
 export default App
