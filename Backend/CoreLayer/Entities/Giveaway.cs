bdf61fbd14ae22a62e786cd66b1eff376741dde4
Başar Arda Baykal <baykalbasararda@gmail.com>
2025-11-30 11:42:51 +0300

    fixed default value of IsActive

--- Backend/CoreLayer/Entities/Giveaway.cs
+++ Backend/CoreLayer/Entities/Giveaway.cs
@@ -7,7 +7,7 @@ public class Giveaway : IBaseEntity
     public string[]? Substitutes { get; set; }
     public int NumberOfWinners { get; set; }
     public int NumberOfSubstitutes { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } = true;
 
     public ICollection<Participator> Participators { get; set; } = new List<Participator>();
 }
